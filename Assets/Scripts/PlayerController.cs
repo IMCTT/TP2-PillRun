7830101c20c5fab6da36a3bce291d4921e86670d
IMCTT <tomas.tournon@estudiante.imagecampus.edu.ar>
2026-05-20 19:39:25 -0300

    Mapa Gameply basico

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,22 +16,33 @@ public class PlayerController : NetworkBehaviour
     [Networked] public int CheckpointIndex { get; set; }
     [Networked] public NetworkBool IsFinished { get; set; }
 
+  
     private void Awake()
     {
-        // busco el componente en el mismo objeto
+        
         ncc = GetComponent<NetworkCharacterController>();
     }
-
     
+    public override void Spawned()
+    {
+        Debug.Log("Spawned! HasInputAuthority: " + HasInputAuthority);
+    
+        if (HasInputAuthority)
+        {
+            var inputHandler = GetComponent<InputHandler>();
+            Debug.Log("InputHandler encontrado: " + (inputHandler != null));
+            if (inputHandler != null)
+                Runner.AddCallbacks(inputHandler);
+        }
+    }
+
     public override void FixedUpdateNetwork()
     {
         if (GetInput(out NetworkInputData input))
         {
-            
             Vector3 movimiento = new Vector3(input.Direction.x, 0, input.Direction.y) * velocidad;
             ncc.Move(movimiento * Runner.DeltaTime);
 
-            // salta si esta en el piso
             if (input.Jump && ncc.Grounded)
             {
                 ncc.Velocity = new Vector3(ncc.Velocity.x, jumpforce, ncc.Velocity.z);
